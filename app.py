59fe0ebe8fc78130f0e0e037dd67d1057962b24b
igor.ribeiro <igorapalvesribeiro@gmail.com>
2024-05-15 11:50:14 -0300

    database-sqlalchemy - feat: created configs to create table in database

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from database import db
+from models.user import User
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "your_secret_key"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 
-db = SQLAlchemy(app)
+db.init_app(app)
 
 @app.route("/hello-world",methods=["GET"])
 def hello_world():
