4710132c52cdd3062a544f7faeebec1c0f78abaa
igor.ribeiro <igorapalvesribeiro@gmail.com>
2024-05-15 15:58:13 -0300

    authentication-flask-login - feat: created endpoints for searching, updating and deleting users

--- app.py
+++ app.py
@@ -54,9 +54,44 @@ def create_user():
 
     return jsonify({ "message": "Dados inválidos!" }), 400
 
-@app.route("/hello-world",methods=["GET"])
-def hello_world():
-    return "Hello world"
+@app.route("/user/<int:id_user>",methods=["GET"])
+@login_required
+def read_user(id_user):
+    user = User.query.get(id_user)
+
+    if user:
+        return { "username": user.username }
+    
+    return jsonify({ "message": "Usuário não encontrado" }), 404
+
+@app.route("/user/<int:id_user>",methods=["PUT"])
+@login_required
+def update_user(id_user):
+    data = request.json
+    user = User.query.get(id_user)
+
+    if user and data.get("password"):
+        user.password = data.get("password")
+        db.session.commit()
+
+        return jsonify({ "message": f"Usuário {id_user} atualizado com sucesso" })
+    
+    return jsonify({ "message": "Usuário não encontrado" }), 404
+
+@app.route("/user/<int:id_user>",methods=["DELETE"])
+@login_required
+def delete_user(id_user):
+    user = User.query.get(id_user)
+
+    if id_user == current_user.id:
+        return jsonify({ "message": "Deleção não permitida" }), 403
+
+    if user:
+        db.session.delete(user)
+        db.session.commit()
+        return jsonify({ "message": f"Usuário {id_user} deletado com sucesso" })
+    
+    return jsonify({ "message": "Usuário não encontrado" }), 404
 
 if __name__ == "__main__":
     app.run(debug=True)
