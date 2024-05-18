60235fb1529d1dc54a52f42379a888c686cb7de3
igor.ribeiro <igorapalvesribeiro@gmail.com>
2024-05-18 10:48:21 -0300

    finishing-project - feat: created user profile and adjusted API to have access control according to profile

--- app.py
+++ app.py
@@ -47,7 +47,7 @@ def create_user():
     password = data.get("password")
 
     if username and password:
-        user = User(username=username, password=password)
+        user = User(username=username, password=password, role="user")
         db.session.add(user)
         db.session.commit()
         return jsonify({ "message": "Usuário cadastrado com sucesso" })
@@ -70,6 +70,9 @@ def update_user(id_user):
     data = request.json
     user = User.query.get(id_user)
 
+    if id_user != current_user.id and current_user.role == "user": 
+        return jsonify({ "message": "Operação não permitida" }), 403
+    
     if user and data.get("password"):
         user.password = data.get("password")
         db.session.commit()
@@ -82,6 +85,9 @@ def update_user(id_user):
 @login_required
 def delete_user(id_user):
     user = User.query.get(id_user)
+
+    if current_user.role != "admin":
+        return jsonify({ "message": "Operação não permitida" }),403
 
     if id_user == current_user.id:
         return jsonify({ "message": "Deleção não permitida" }), 403
