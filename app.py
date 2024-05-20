7fc6bdafa9566ca25376f871f4faebdcd3225726
igor.ribeiro <igorapalvesribeiro@gmail.com>
2024-05-20 16:14:41 -0300

    finishing-project - feat: added to encrypt passwords before saving to database

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 from database import db
 from models.user import User
 from flask_login import LoginManager, login_user, current_user, logout_user, login_required
+import bcrypt
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "your_secret_key"
@@ -27,7 +28,7 @@ def login():
     if username and password:
         user = User.query.filter_by(username=username).first()
 
-        if user and user.password == password:
+        if user and bcrypt.checkpw(str.encode(password), str.encode(user.password)):
             login_user(user)
             print(current_user.is_authenticated)
             return jsonify({ "message": "Autenticação realizada com sucesso" }), 200
@@ -47,7 +48,8 @@ def create_user():
     password = data.get("password")
 
     if username and password:
-        user = User(username=username, password=password, role="user")
+        hashed_password = bcrypt.hashpw(str.encode(password), bcrypt.gensalt())
+        user = User(username=username, password=hashed_password, role="user")
         db.session.add(user)
         db.session.commit()
         return jsonify({ "message": "Usuário cadastrado com sucesso" })
@@ -74,7 +76,8 @@ def update_user(id_user):
         return jsonify({ "message": "Operação não permitida" }), 403
     
     if user and data.get("password"):
-        user.password = data.get("password")
+        hashed_password = bcrypt.hashpw(str.encode(data.get("password")), bcrypt.gensalt())
+        user.password = hashed_password
         db.session.commit()
 
         return jsonify({ "message": f"Usuário {id_user} atualizado com sucesso" })
